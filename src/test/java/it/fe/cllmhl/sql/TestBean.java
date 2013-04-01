3bbb6cff3599a443008a931237c9b95a8b0fce42
cllmhl <private>
2013-04-01 12:04:12 +0200

    Cleanup

--- src/test/java/it/fe/cllmhl/sql/TestBean.java
+++ src/test/java/it/fe/cllmhl/sql/TestBean.java
@@ -1,54 +1,59 @@
 package it.fe.cllmhl.sql;
 
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+
 
 public final class TestBean extends BaseDaoBean {
 
-	private java.lang.Integer chiave;
-	private java.lang.String stringa;
-	private java.math.BigDecimal importo;
-	private java.lang.Integer qty;
-	private java.sql.Date data, datato; 
+	private Integer chiave;
+	private String stringa;
+	private BigDecimal importo;
+	private Integer qty;
+	private Date data, datato; 
 
 
-	public java.lang.Integer getChiave() {
+	public Integer getChiave() {
 		return chiave;
 	}
-	public void setChiave(java.lang.Integer chiave) {
+	public void setChiave(Integer chiave) {
 		this.chiave = chiave;
 	}
 
-	public java.lang.String getStringa() {
+	public String getStringa() {
 		return stringa;
 	}
-	public void setStringa(java.lang.String stringa) {
+	public void setStringa(String stringa) {
 		this.stringa = stringa;
 	}
 
-	public java.math.BigDecimal getImporto() {
+	public BigDecimal getImporto() {
 		return importo;
 	}
-	public void setImporto(java.math.BigDecimal importo) {
+	public void setImporto(BigDecimal importo) {
 		this.importo = importo;
 	}
 
-	public java.lang.Integer getQty() {
+	public Integer getQty() {
 		return qty;
 	}
-	public void setQty(java.lang.Integer qty) {
+	public void setQty(Integer qty) {
 		this.qty = qty;
 	}
 
-	public java.sql.Date getData() {
+	public Date getData() {
 		return data;
 	}
-	public void setData(java.sql.Date data) {
+	public void setData(Date data) {
 		this.data = data;
 	}
  
-	public java.sql.Date getDatato() {
+	public Date getDatato() {
 		return datato;
 	}
-	public void setDatato(java.sql.Date datato) {
+	public void setDatato(Date datato) {
 		this.datato = datato;
 	} 
 }
